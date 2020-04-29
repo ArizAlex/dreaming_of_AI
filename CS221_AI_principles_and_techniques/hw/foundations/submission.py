46cc46337f517a7380e38c39cf93be6639560eed
ArizAlex <arizalexp@gmail.com>
2020-04-28 21:55:16 -0500

    finishes hw 01

--- CS221_AI_principles_and_techniques/hw/foundations/submission.py
+++ CS221_AI_principles_and_techniques/hw/foundations/submission.py
@@ -12,7 +12,7 @@ def findAlphabeticallyLastWord(text):
     You might find max() and list comprehensions handy here.
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return sorted(text.split())[-1]
     # END_YOUR_CODE
 
 ############################################################
@@ -24,13 +24,13 @@ def euclideanDistance(loc1, loc2):
     are pairs of numbers (e.g., (3, 5)).
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return math.sqrt((loc1[1]-loc2[1])**2+(loc1[0]-loc2[0])**2)
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3c
 
-def mutateSentences(sentence):
+def mutateSentences(st):
     """
     Given a sentence (sequence of words), return a list of all "similar"
     sentences.
@@ -50,33 +50,51 @@ def mutateSentences(sentence):
                 (reordered versions of this list are allowed)
     """
     # BEGIN_YOUR_CODE (our solution is 20 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+    done, length = [], len(st.split())
+    def alike(num):
+        return [i for i,w in enumerate(st.split()) if st.split()[num] == w]
+    def fun(i,branch_at_i):
+        if len(branch_at_i) == length:
+            done.append(branch_at_i)
+            return ''
+        if i == length-1:
+            return ''
+        al = alike(i)
+        if len(al)>1:
+            for j in al:
+                fun(j+1,branch_at_i+f'{j+1}')
+            return ''
+        fun(i+1,branch_at_i+f'{i+1}')
+    [fun(i,f'{i}') for i in range(length)]
+    return list({' '.join(st.split()[int(i)] for i in comb) for comb in done})
 
 ############################################################
 # Problem 3d
 
-def sparseVectorDotProduct(v1, v2):
+def sparseVectorDotProduct(a, b):
     """
-    Given two sparse vectors |v1| and |v2|, each represented as collections.defaultdict(float), return
+    Given two sparse vectors |a| and |b|, each represented as collections.defaultdict(float), return
     their dot product.
     You might find it useful to use sum() and a list comprehension.
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    ref = a if len(a)>len(b) else b
+    return sum(a[key]*b[key] for key in ref)
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3e
 
-def incrementSparseVector(v1, scale, v2):
+def incrementSparseVector(a, scale, b):
     """
     Given two sparse vectors |v1| and |v2|, perform v1 += scale * v2.
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    ref = a if len(a)>len(b) else b
+    for key in ref:
+        a[key] += scale*b[key]
     # END_YOUR_CODE
 
 ############################################################
@@ -89,7 +107,7 @@ def findSingletonWords(text):
     You might find it useful to use collections.defaultdict(int).
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return {w for w in text.split() if text.count(w) == 1}
     # END_YOUR_CODE
 
 ############################################################
@@ -105,5 +123,14 @@ def computeLongestPalindromeLength(text):
     You should first define a recurrence before you start coding.
     """
     # BEGIN_YOUR_CODE (our solution is 19 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    def isPal(t):
+        if len(t) == 1:
+            return True
+        if t[0] == t[-1] and isPal(t[1:-1]):
+            return True
+        return False
+    for size in range(len(text),0,-1):
+        for c in combinations(s,size):
+            if isPal(''.join(l for l in c)):
+                return ''.join(l for l in c)
     # END_YOUR_CODE
